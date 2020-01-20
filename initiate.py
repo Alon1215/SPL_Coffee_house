8d3b0e520d64b69a5cffc35871f3dedf510ce508
oferMoses <10ofer0@gmail.com>
2020-01-20 14:07:01 +0200

    Ofer: initiate

--- initiate.py
+++ initiate.py
@@ -3,6 +3,8 @@ from DAOs import _CoffeeStands,_Activities,_Employees,_Products,_Suppliers
 from DTOs import *
 from REPOSITORY import repo
 
+repo.create_tables()
+
 file = open('config.txt', 'r')
 f = file.readlines()
 parseList = []
@@ -18,16 +20,14 @@ print(parseList)
 
 for line in parseList:
     if(line[0]=='C'):
-        _CoffeeStands.insert(CoffeeStand())
-
-
-
+        _CoffeeStands.insert(CoffeeStand(*line[1:]))
     elif line[0] == 'S':
-
+        _Suppliers.insert(Supplier(*line[1:]))
     elif line[0] == 'E':
         #_Employees.insert(Employee(line[1],line[2],line[3],line[3]))
-        _Employees.insert(Employee(*line[1:]))
-    elif line[0] == 'F':
+        _Employees.insert(Employee(*line[1:])) #TODO:check if this works
+    elif line[0] == 'P':
+        _Products.insert(Product(*line[1:]))
 
 
 
